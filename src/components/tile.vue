f48500c5e58c5dd6c0371733e660ce468cc813ca
banasikp <przemyslaw.banasik@gmail.com>
2025-01-15 23:05:53 +0100

    first component

---
+++ src/components/tile.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+     x: number
+    y: number,
+    z: number,
+    value: number
+}>()
+</script>
+
+<template>
+  <div class="tile">
+    {{value}}
+  </div>
+</template>
+
+<style scoped>
+    .tile
+    {        
+        background-color: red;
+        border: 1px solid #fff;
+        color: #fff;
+        font-size: 35px;
+        width: 70px;
+        height: 70px;
+
+
+        display: flex;
+        justify-content: center;
+        align-items: center;        
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+    import  tile from '../components/tile.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div id="sudoku-container">
+        <tile :x="0" :y="0" :z="0" :value="2" />
+        <tile :x="0" :y="1" :z="0" :value="1" />
+        <tile :x="0" :y="2" :z="0" :value="3" />
+    </div>
 </template>
